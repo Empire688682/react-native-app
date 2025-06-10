18c8ccc8491d12471348384b94acad28c97e291d
juwon <asehindej@gmail.com>
2025-06-10 17:58:04 +0100

    Refactor tab layout and remove unused links from index screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,13 @@
 import { Tabs } from "expo-router";
+import Feather from '@expo/vector-icons/Feather';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Entypo from '@expo/vector-icons/Entypo';
 
 export default function RootLayout() {
-  return <Tabs>
-    <Tabs.Screen name="index" options={{title: "Home"}} />
-    <Tabs.Screen name="login" options={{title: "Login"}} />
-    <Tabs.Screen name="about" options={{title: "About"}} />
-  </Tabs>;
+    return <Tabs screenOptions={{tabBarActiveTintColor:"green", tabBarActiveBackgroundColor:"coral", headerTitleAlign:"center"}}>
+        <Tabs.Screen name="index" options={{ title: "Home", tabBarIcon: ({color})=><Feather name="home" size={24} color={color} /> }} />
+        <Tabs.Screen name="login" options={{ title: "Login", tabBarIcon: ({color})=> <AntDesign name="login" size={24} color={color} /> }} />
+        <Tabs.Screen name="about" options={{ title: "About", tabBarIcon: ({color}) => <Entypo name="info-with-circle" size={24} color={color} /> }} />
+    </Tabs>;
 }
 
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,9 @@
 import { Text, View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.welcomeText}>Welcome!</Text>
-      <Link href="/login">Login</Link>
-      <Link href="/about">About</Link>
     </View>
   );
 };
