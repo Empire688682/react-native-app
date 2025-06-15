714ce90e0e5d4dda2619e3db6686abd312c21b7b
juwon <asehindej@gmail.com>
2025-06-15 18:02:59 +0100

    center

---
+++ app/(tabs)/index.jsx
@@ -0,0 +1,24 @@
+import { 
+  Text, 
+  View, 
+  StyleSheet, 
+} from "react-native";
+
+export default function Index() {
+
+  return (
+    <View style={styles.container}>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: '#f8f9fa',
+  },
+});
